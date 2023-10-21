d7572895538198ae53d6095964214ea745156e16
Mandeep Singh <shaan9888@gmail.com>
2023-10-20 20:32:25 -0400

    router lab

--- src/App.js
+++ src/App.js
@@ -63,46 +63,77 @@
 
 // export default App;
 
-import "./App.css";
-import Transfer from "./Transfer";
-import Receipt from "./Reciept";
-import React, { useState } from "react";
+// import "./App.css";
+// import Transfer from "./Transfer";
+// import Receipt from "./Reciept";
+// import React, { useState } from "react";
+
+// function App() {
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const [transactionData, setTransactionData] = useState({});
+
+//   const handleTransfer = ({ amount, from, to }) => {
+//     if (amount < 0 || amount === 0 || amount === "") {
+//       alert("Enter valid amount");
+//       return;
+//     }
+//     setTransactionData({
+//       amount,
+//       from,
+//       to,
+//       transactionDate: new Date().toDateString(),
+//       transactionID:
+//         "0xb4bc263278d3f77a652a8d73a6bfd8ec0ba1a63923bbb4f38147fb8a943da26d",
+//     });
+//     setIsModalOpen(true);
+//   };
+//   return (
+//     <div className="App">
+//       <Transfer
+//         openReceipt={({ amount, from, to }) =>
+//           handleTransfer({ amount, from, to })
+//         }
+//       />
+//       {isModalOpen && (
+//         <Receipt
+//           closeModal={() => {
+//             setIsModalOpen(false);
+//           }}
+//           receiptData={transactionData}
+//         />
+//       )}
+//     </div>
+//   );
+// }
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Routes,
+} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contract";
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [transactionData, setTransactionData] = useState({});
-
-  const handleTransfer = ({ amount, from, to }) => {
-    if (amount < 0 || amount === 0 || amount === "") {
-      alert("Enter valid amount");
-      return;
-    }
-    setTransactionData({
-      amount,
-      from,
-      to,
-      transactionDate: new Date().toDateString(),
-      transactionID:
-        "0xb4bc263278d3f77a652a8d73a6bfd8ec0ba1a63923bbb4f38147fb8a943da26d",
-    });
-    setIsModalOpen(true);
-  };
   return (
-    <div className="App">
-      <Transfer
-        openReceipt={({ amount, from, to }) =>
-          handleTransfer({ amount, from, to })
-        }
-      />
-      {isModalOpen && (
-        <Receipt
-          closeModal={() => {
-            setIsModalOpen(false);
-          }}
-          receiptData={transactionData}
-        />
-      )}
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <Routes>
+          <Route path="/" exact Component={Home}></Route>
+          <Route path="/about" Component={About}></Route>
+          <Route path="/contact" Component={Contact}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import "./index.css";
+const Home = () => {
+  return (
+    <>
+      <div>This is Home page </div>
+    </>
+  );
+};
+
+export default Home;
